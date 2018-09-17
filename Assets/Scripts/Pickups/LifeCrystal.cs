b6df60cd428229615ecb7456fffc96e8cbc73acd
18강현모 <hyunmokang@naver.com>
2018-09-17 22:24:55 +0900

    프로젝트에서 사용할 오브젝트와 코드 생성. 현모 작업부분 변수&메소드 설계.

---
+++ Assets/Scripts/Pickups/LifeCrystal.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeCrystal : MonoBehaviour {
+
+    public int[][] grid;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    /// <summary>
+    /// Randomly upgrade cells
+    /// </summary>
+    /// <param name="tier">Tier</param>
+    public void Upgrade(int tier)
+    {
+    }
+
+}
---
+++ Assets/Scripts/UI/LifeCrystalUI.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Healthbar manager
+/// </summary>
+public class LifeCrystalUI : MonoBehaviour {
+
+
+    /// <summary>
+    /// Current life total
+    /// </summary>
+    int lifeCount
+    {
+        get { return lifeCount; }
+        set { }
+    }
+
+    int goldCount
+    {
+        get { return goldCount; }
+        set { }
+    }
+	
+	void Start ()
+    {
+		
+	}
+	
+	
+	void Update ()
+    {
+		
+	}
+
+
+
+    /// <summary>
+    /// Take given amount of damage
+    /// </summary>
+    /// <param name="dmg">Amount of damage to take</param>
+    public void TakeDamege(int dmg)
+    {
+        
+    }
+    
+    /// <summary>
+    /// Gain given amount of gold
+    /// </summary>
+    /// <param name="gold">Amount of gold to gain</param>
+    public void GainGold(int gold)
+    {
+
+    }
+
+    /// <summary>
+    /// Gain a LifeCrystal
+    /// </summary>
+    /// <param name="frag">LifeCrystal to gain</param>
+    /// <returns>returns false if no cell taken, true otherwise</returns>
+    public bool GainLifeCrystal(LifeCrystal frag)
+    {
+        return false;
+    }
+
+    /// <summary>
+    /// Use given amount of gold
+    /// </summary>
+    /// <param name="gold">Amount of gold to use</param>
+    /// <returns>Returns false when money not enough, true otherwise</returns>
+    public bool UseGold(int gold)
+    {
+        return false;
+    }
+
+}
