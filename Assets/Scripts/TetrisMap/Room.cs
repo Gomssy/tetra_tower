98380d967746b55b9ffd26e54b4bc02b8a07500a
18손재민 <jasson31@naver.com>
2018-09-19 12:37:09 +0900

    일단 급한대로 Room 추가합니다.

--- Assets/Scripts/TetrisMap/Room.cs
+++ Assets/Scripts/TetrisMap/Room.cs
@@ -2,15 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Room : MonoBehaviour {
+/// <summary>
+/// Room class
+/// </summary>
+public class Room : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    /// <summary>
+    /// Room's Location on tetris map.
+    /// Not related to real location.
+    /// </summary>
+    public Vector3 mapLocation;
+
+    /// <summary>
+    /// Door info per rooms.
+    /// 0 for up, 1 for down.
+    /// doorInfo[0] for left door, doorInfo[1] for right door.
+    /// </summary>
+    public int[] doorInfo;
+
+    /// <summary>
+    /// Stage per rooms.
+    /// Range from 0 to 4.
+    /// </summary>
+    public int stage;
+
+    /// <summary>
+    /// Room concept per rooms.
+    /// Range from 0 to 3.
+    /// </summary>
+    public int roomConcept;
+
+    /// <summary>
+    /// Item room type per rooms.
+    /// 0 for normal room, 1~ for item rooms.
+    /// </summary>
+    public int itemRoomType;
+
+    /// <summary>
+    /// Special room type per rooms.
+    /// 0 for normal room, 1 for boss room, 2, 3, 4 for both side, gold shop, amethyst shop.
+    /// </summary>
+    public int specialRoomType;
+
+    /// <summary>
+    /// Select which doors would be opened.
+    /// </summary>
+    public void SetDoors()
+    {
+
+    }
+
+    void Awake()
+    {
+        doorInfo = new int[2] { 0, 0 };
+    }
+
 }
