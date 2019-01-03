6418a99605c989c294a8bc3826e3e7c82c5a85d6
김상언 <gggomksy@naver.com>
2019-01-03 17:18:17 +0900

    EnemyManager의 LoadDropTable() 코드 작성함 민규 킴의 다음 지시를 머기하겠음

--- Assets/Scripts/Characters/EnemyManager.cs
+++ Assets/Scripts/Characters/EnemyManager.cs
@@ -11,7 +11,7 @@ public class EnemyManager : Singleton<EnemyManager> {
 
     public delegate void Action<T>();
 
-    private Dictionary<uint, List<int[]>> dropTableByID;
+    private Dictionary<uint, int[]> dropTableByID;
     private Dictionary<uint, List<int[]>> actionDictByID;
 
     protected EnemyManager() {
@@ -21,9 +21,27 @@ public class EnemyManager : Singleton<EnemyManager> {
         LoadDropTable(dropTableDataPath);
         LoadActionTable(actionTableDataPath);
     }
-    private void LoadDropTable(string dataPath) {
+   
+    private void LoadDropTable(string dataPath)
+    {
+        StreamReader strReader = new StreamReader(dataPath, Encoding.UTF8);
+        string[] cellValue = null; //csv파일 한 행에 포함되는 칸들의 값 넣을 배열
+        string tableLine = null; //파일 한 행
+        strReader.ReadLine(); //첫 줄 스킵
 
+        while (tableLine = strReader.ReadLine() != null) 
+        {
+            if (string.IsNullOrEmpty(tableLine)) return; //행이 비었는지 체크
+
+            cellValue = tableLine.Split(',');
+            int monsterID = cellValue[0]; //monster가 가진 ID
+            int itemID = cellValue[2]; //드랍하는 item의 ID
+            int dropWeight = cellValue[3]; //드랍할 확률
+            int[] itemDrop = new int[] { itemID, dropWeight };
+            dropTableByID.Add(monsterID, itemDrop);
+        }
     }
+
     private void LoadActionTable(string dataPath) {
 
     }
