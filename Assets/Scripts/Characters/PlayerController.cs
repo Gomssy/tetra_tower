f970dda980faac7ff885071693bbd23993dc102a
abpo11 <syncbak123@gmail.com>
2019-01-07 19:48:57 +0900

    플레이어가 로프를 탈 수 있게됨. 로프에 닿은 상태에서 점프시 타고, 좌우 방향키만 누를시 내림

--- Assets/Scripts/Characters/PlayerController.cs
+++ Assets/Scripts/Characters/PlayerController.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
     private Rigidbody2D rb;     // RigidBody2D of this game object
 
     // Speeds of player
@@ -15,34 +16,42 @@ public class PlayerController : MonoBehaviour {
     [SerializeField]
     private float jumpSpeed;
     [SerializeField]
-    
+    private float ropeSpeed;
+    [SerializeField]
     private float doubleJumpSpeed;
-   
+
     // Bool values for jump & doublejump
     private bool isGrounded = true;
     private bool isJumpable = true;     // Can player jump or doublejump?
-
+    private bool isInRope = false;
     // Inputs
     private float horizontal = 0;
     private float horizontalRaw = 0;
+    private float verticalRaw = 0;
     private bool jump = false;
 
     // Variables for IsGrounded()
     [SerializeField]
     private LayerMask groundLayer;
     [SerializeField]
+    private LayerMask ropeLayer;
+    [SerializeField]
+    private float ropeDistance = 0.3f;
+    [SerializeField]
     private float rayDistance;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         rb = gameObject.GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         horizontal = Input.GetAxis("Horizontal");
         horizontalRaw = Input.GetAxisRaw("Horizontal");
-
+        verticalRaw = Input.GetAxisRaw("Vertical");
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
@@ -55,32 +64,59 @@ public class PlayerController : MonoBehaviour {
         if (isGrounded)
             isJumpable = true;
 
-        float vertical = rb.velocity.y;
-        if (jump)
+        if (IsInRope())
         {
-            if (isGrounded)
+            if (isInRope)
             {
-                vertical = jumpSpeed * Time.fixedDeltaTime;
+                if (horizontalRaw != 0f && verticalRaw == 0f)
+                {
+                    isInRope = false;
+                    rb.gravityScale = 2f;
+                }
+                rb.velocity = new Vector2(0f, verticalRaw * Time.smoothDeltaTime * ropeSpeed);
+
             }
-            else if (isJumpable)
+            else if (jump)
             {
-                vertical = doubleJumpSpeed * Time.fixedDeltaTime;
-                isJumpable = false;
+                isInRope = true;
+                rb.gravityScale = 0f;
+                transform.position = new Vector2(Mathf.Round(transform.position.x - 0.5f) + 0.5f, transform.position.y);
+                rb.velocity = new Vector2(0f, 0f);
             }
         }
-        
-            //rb.velocity = new Vector2(horizontal * speed * Time.fixedDeltaTime, vertical);
-           // rb.velocity = new Vector2(rb.velocity.x, vertical);
-            rb.AddForce(horizontalRaw * speed * Time.fixedDeltaTime * Vector2.right);
-            if (((horizontalRaw==0) || (rb.velocity.x > 0 && horizontalRaw < 0)
-                || (rb.velocity.x < 0 && horizontalRaw > 0)) &&(isGrounded))
+        else
+        {
+            isInRope = false;
+            rb.gravityScale = 2f;
+        }
+        if (!isInRope)
+        {
+            float vertical = rb.velocity.y;
+            if (jump)
             {
-           
+                if (isGrounded)
+                {
+                    vertical = jumpSpeed *  Time.smoothDeltaTime;
+                }
+                else if (isJumpable)
+                {
+                    vertical = doubleJumpSpeed *  Time.smoothDeltaTime;
+                    isJumpable = false;
+                }
+            }
+
+            //rb.velocity = new Vector2(horizontal * speed *  Time.smoothDeltaTime, vertical);
+            // rb.velocity = new Vector2(rb.velocity.x, vertical);
+            rb.AddForce(horizontalRaw * speed * Time.smoothDeltaTime * Vector2.right);
+            if (((horizontalRaw == 0) || (rb.velocity.x > 0 && horizontalRaw < 0)
+                || (rb.velocity.x < 0 && horizontalRaw > 0)) && (isGrounded))
+            {
+
                 rb.AddForce(rb.velocity.x * (-10f) * Vector2.right);
             }
-                
-        rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -maxSpeed*Time.fixedDeltaTime, maxSpeed * Time.fixedDeltaTime), vertical);
 
+            rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -maxSpeed * Time.smoothDeltaTime, maxSpeed * Time.smoothDeltaTime), vertical);
+        }
         jump = false;
     }
     bool IsGrounded()   // Is player grounded?
@@ -88,5 +124,13 @@ public class PlayerController : MonoBehaviour {
         RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, rayDistance, groundLayer);
         Debug.DrawRay(transform.position, rayDistance * Vector2.down, Color.white);
         return hit.collider != null;
+    }
+    bool IsInRope()   // Is player in rope?
+    {
+        RaycastHit2D hit1 = Physics2D.Raycast(transform.position, Vector2.right, ropeDistance, ropeLayer);
+        RaycastHit2D hit2 = Physics2D.Raycast(transform.position, Vector2.left, ropeDistance, ropeLayer);
+        Debug.DrawRay(transform.position, ropeDistance * Vector2.right, Color.red);
+        Debug.DrawRay(transform.position, ropeDistance * Vector2.left, Color.red);
+        return hit1.collider != null || hit2.collider != null;
     }
 }
