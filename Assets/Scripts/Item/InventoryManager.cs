f30a49c8ba45bbf7c0d51142a539396c9b953743
18류지석 <roadmageb@naver.com>
2019-01-28 21:43:07 +0900

    드랍된 아이템 구현

--- Assets/Scripts/Item/InventoryManager.cs
+++ Assets/Scripts/Item/InventoryManager.cs
@@ -9,6 +9,7 @@ public class InventoryManager : MonoBehaviour {
     InventoryUI ui;
     public List<string> itemPool = new List<string>();
     public List<string> addonPool = new List<string>();
+    public GameObject droppedPrefab;
 
     private void Start()
     {
@@ -16,13 +17,16 @@ public class InventoryManager : MonoBehaviour {
         GameObject.Find("InventoryCanvas").SetActive(false);
 
         //itemPool
-        itemPool.Add("Bow");
+        itemPool.Add("Baculus");
         itemPool.Add("BambooSpear");
-        itemPool.Add("Ksanife");
+        itemPool.Add("Bow");
+        itemPool.Add("ChainSickle");
+        itemPool.Add("Festo");
 
         //addonPool
         addonPool.Add("ApprenticesMark");
         addonPool.Add("ParchmentPiece");
+        addonPool.Add("GlowingHerb");
 
         StartCoroutine("TestCoroutine");
     }
@@ -32,8 +36,19 @@ public class InventoryManager : MonoBehaviour {
         yield return null;
         PushItem((Item)System.Activator.CreateInstance(System.Type.GetType(itemPool[0])));
         PushItem((Item)System.Activator.CreateInstance(System.Type.GetType(itemPool[2])));
-        /*yield return new WaitForSeconds(1f);
-        ItemSelect(0);
+        yield return new WaitForSeconds(1.5f);
+        ItemInstantiate(itemPool[0], GameObject.Find("Player").transform.position);
+        yield return new WaitForSeconds(1.5f);
+        ItemInstantiate(itemPool[1], GameObject.Find("Player").transform.position);
+        yield return new WaitForSeconds(1.5f);
+        ItemInstantiate(itemPool[2], GameObject.Find("Player").transform.position);
+        yield return new WaitForSeconds(1.5f);
+        ItemInstantiate(itemPool[3], GameObject.Find("Player").transform.position);
+        yield return new WaitForSeconds(1.5f);
+        AddonInstantiate(addonPool[0], GameObject.Find("Player").transform.position);
+        yield return new WaitForSeconds(1.5f);
+        AddonInstantiate(addonPool[1], GameObject.Find("Player").transform.position);
+        /*ItemSelect(0);
         yield return new WaitForSeconds(1f);
         PushItem((Item)System.Activator.CreateInstance(System.Type.GetType(itemPool[1])));
         yield return new WaitForSeconds(1f);
@@ -47,6 +62,16 @@ public class InventoryManager : MonoBehaviour {
         yield return new WaitForSeconds(1f);
         */
     }
+    public void ItemInstantiate(string str, Vector3 pos)
+    {
+        GameObject tmpItem = Instantiate(droppedPrefab);
+        tmpItem.GetComponent<DroppedItem>().Init((Item)System.Activator.CreateInstance(System.Type.GetType(str)), pos);
+    }
+    public void AddonInstantiate(string str, Vector3 pos)
+    {
+        GameObject tmpItem = Instantiate(droppedPrefab);
+        tmpItem.GetComponent<DroppedItem>().Init((Addon)System.Activator.CreateInstance(System.Type.GetType(str)), pos);
+    }
     public void SetOnPosition()
     {
         ui.SetOnPosition(itemList, addonList);
@@ -56,17 +81,27 @@ public class InventoryManager : MonoBehaviour {
         ui.selectedItem = itemIndex;
         ui.SetOnPosition(itemList, addonList);
     }
-    public void PushItem(Item item)
+    public bool PushItem(Item item)
     {
-        if(itemList.Count < 9)
-            itemList.Add(item);
+        for(int i = 0; i<item.skillNum; i++)
+            foreach (Item tmpItem in itemList)
+                for (int j = 0; j < tmpItem.skillNum; j++)
+                    if (item.combo[i].Equals(tmpItem.combo[j]))
+                        return false;
+
+        if (itemList.Count > 8) return false;
+
+        itemList.Add(item);
         ui.SetOnPosition(itemList, addonList);
+        return true;
     }
-    public void PushAddon(Addon addon)
+    public bool PushAddon(Addon addon)
     {
-        if (addonList.Count < 9)
-            addonList.Add(addon);
+        if (addonList.Count > 8) return false;
+
+        addonList.Add(addon);
         ui.SetOnPosition(itemList, addonList);
+        return true;
     }
     public void DiscardItem(int index)
     {
