29451a7a1eef0959de74a560ff7cdbb3ddee7b8a
airforce <mk970205@gmail.com>
2019-02-15 16:11:44 +0900

    누락사항 넣음

---
+++ Assets/Scripts/Characters/DamageToPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageToPlayer : MonoBehaviour {
+    public int damage;
+    // enum debuff
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("Auch!");
+        }
+    }
+}
