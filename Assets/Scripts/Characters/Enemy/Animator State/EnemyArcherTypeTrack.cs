ea179f6da23905cea21cb4960061e23143b0a72a
김상언 <gggomksy@naver.com>
2019-02-24 20:52:23 +0900

    일단 개 븅딱같이 작동하는 졸라맨아쳐를 만들었다. 애니메이션 나오면 체력, 대미지, 화살 속도, 애니메이션 시전 속도 등등 수정해야함. 그리고 일단 얘는 화살이 활에서부터 발사되게 Body의 하위 오브젝트로 ShootPoint를 만들어서 거기서 발사 되게 했다. 플레이어는 화살이 가슴에서 나간다. 아이언맨인 듯 하다. 그리고 생각해보니 넉백 만들어야함.

---
+++ Assets/Scripts/Characters/Enemy/Animator State/EnemyArcherTypeTrack.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyArcherTypeTrack : StateMachineBehaviour
+{
+    float trackSpeed;
+    float attackRange;
+    GameObject player;
+    Transform animatorRoot;
+    Enemy enemy;
+    GameObject jollarcher_arrow;
+    EnemyArrow enemyArrow;
+
+
+    readonly int maxFrame = 10;
+    int frameCounter = 0;
+
+    float waitBetweenShots;
+    private float shotCounter;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        jollarcher_arrow = Resources.Load<GameObject>("Prefabs/Projectiles/jollarcher_arrow");
+        waitBetweenShots = 0.1f;
+        shotCounter = waitBetweenShots;
+
+        animatorRoot = animator.transform.parent;
+        enemy = animator.GetComponent<Enemy>();
+        player = EnemyManager.Instance.Player;
+
+
+        trackSpeed = enemy.trackSpeed;
+        attackRange = enemy.attackRange;
+
+        NumeratedDir trackDir = (animatorRoot.position.x - player.transform.position.x > 0) ? NumeratedDir.Left : NumeratedDir.Right;
+        enemy.ChangeDir(trackDir);
+        if (enemy.CliffTest[(enemy.MoveDir + 1) / 2] || animator.GetComponent<Enemy>().PlayerDistance < attackRange)
+        {
+            enemy.ChangeVelocityX(0.0f);
+        }
+        else
+        {
+            enemy.ChangeVelocityX(enemy.MoveDir * trackSpeed);
+        }
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        shotCounter -= Time.deltaTime;
+        if (animator.GetComponent<Enemy>().PlayerDistance < attackRange && shotCounter < 0)
+        { 
+            animator.SetTrigger("AttackTrigger");
+            if (player.transform.position.x < enemy.transform.parent.position.x)
+            {
+                Vector2 direction =  enemy.transform.GetChild(0).position-player.transform.position;
+                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+                Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+                
+                Instantiate(jollarcher_arrow, enemy.transform.GetChild(0).position, rotation);
+            }
+            else
+            {
+                Vector2 direction =  enemy.transform.GetChild(0).position-player.transform.position;
+                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+                Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+                Instantiate(jollarcher_arrow, enemy.transform.GetChild(0).position, rotation);
+            }
+            shotCounter = waitBetweenShots;
+            return;
+        }
+        int integerDir = enemy.MoveDir;
+        if (enemy.WallTest[(integerDir + 1) / 2] || enemy.CliffTest[(integerDir + 1) / 2])
+        {
+            enemy.ChangeVelocityX(0.0f);
+        }
+        else
+        {
+            enemy.ChangeVelocityX(enemy.MoveDir * trackSpeed);
+        }
+
+        frameCounter += 1;
+        if (frameCounter >= maxFrame)
+        {
+            NumeratedDir trackDir = (animatorRoot.position.x - player.transform.position.x > 0) ? NumeratedDir.Left : NumeratedDir.Right;
+            enemy.ChangeDir(trackDir);
+            frameCounter = 0;
+        }
+    }
+
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+}
