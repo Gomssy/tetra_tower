fca793f260764a969dcc5b5a2f88f7249baf2d7c
김상언 <gggomksy@naver.com>
2019-02-25 11:54:22 +0900

    화살에 유도기능 달린 Jollarcher2를 만들었다. 스크립트랑 몹 이름은 나중에 바꾸면 되니까 뭐라 ㄴㄴ 암튼 화살이 따라옴. 유도 기능의 성능을 조절하려면 Jollarcher2Arrow 코드에서 moveDirection의 곱하기 값을 바꾸면 됨. (지금은 앞에 0.01f 뒤에 0.99f 곱해져 있는데 앞에 것 값을 크게하면 유도기능이 향상됨.)

---
+++ Assets/Scripts/Characters/Enemy/Animator State/Jollarcher2Idle.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jollarcher2Idle : StateMachineBehaviour
+{
+    float patrolRange;
+    float patrolSpeed;
+    float noticeRange;
+    Vector2 origin;
+    Transform animatorRoot;
+    Enemy enemy;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        origin = animator.transform.position;
+        animatorRoot = animator.transform.parent;
+        enemy = animator.GetComponent<Enemy>();
+
+        patrolRange = enemy.patrolRange;
+        noticeRange = enemy.noticeRange;
+        patrolSpeed = enemy.patrolSpeed;
+
+        enemy.ChangeDir(NumeratedDir.Left);
+        enemy.ChangeVelocityX(enemy.MoveDir * patrolSpeed);
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (enemy.PlayerDistance < noticeRange)
+        {
+            animator.SetTrigger("TrackTrigger");
+            return;
+        }
+        if (!enemy.MovementLock)
+        {
+            float span = animatorRoot.position.x - origin.x;
+
+            if ((Mathf.Abs(span) > patrolRange && span * enemy.MoveDir > 0) ||
+                enemy.WallTest[(enemy.MoveDir + 1) / 2] ||
+                enemy.CliffTest[(enemy.MoveDir + 1) / 2]
+            )
+            {
+                enemy.ChangeDir(enemy.MoveDir * -1);
+                enemy.ChangeVelocityX(enemy.MoveDir * patrolSpeed);
+            }
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+}
---
+++ Assets/Scripts/Characters/Enemy/Jollarcher2Arrow.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jollarcher2Arrow : MonoBehaviour
+{
+
+    Enemy enemy;
+    public float moveSpeed;
+    public float damage = 0;
+    public float knockBackMultiplier = 1f;
+    public LayerMask vanishLayer;
+    public LayerMask stopLayer;
+    Rigidbody2D rb;
+    Player target;
+    Vector2 moveDirection;
+    Vector2 changeAngle;
+    bool checkWall = true;
+
+    void FixedUpdate()
+    {
+        if(checkWall == true)
+        {
+            rb = GetComponent<Rigidbody2D>();
+            target = GameObject.FindObjectOfType<Player>();
+            moveDirection = ((Vector2)(target.transform.position - transform.position).normalized * 0.01f + rb.velocity.normalized * 0.99f) * moveSpeed;
+            rb.velocity = new Vector2(moveDirection.x, moveDirection.y);
+            float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle + 180, Vector3.forward);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        checkWall = false;
+        if (collision.CompareTag("Player"))
+        {
+            EnemyAttackInfo attack = new EnemyAttackInfo(damage, knockBackMultiplier, 0, null, null);
+            collision.gameObject.GetComponent<PlayerAttack>().TakeDamage(attack);
+        }
+
+        if ((vanishLayer == (vanishLayer | 1 << collision.gameObject.layer)))
+        {
+            Destroy(gameObject);
+            
+        }
+
+        if (stopLayer == (stopLayer | 1 << collision.gameObject.layer))
+        {
+            
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            GetComponent<Collider2D>().enabled = false;
+            
+            StartCoroutine(WaitVanish(10f));
+        }
+    }
+    IEnumerator WaitVanish(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(gameObject);
+    }
+}
+
