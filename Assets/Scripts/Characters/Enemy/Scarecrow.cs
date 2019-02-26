206d4042699f0b6b693bb624a67790528c2501c4
mk970205 <mk970205@gmail.com>
2019-02-26 19:01:14 +0900

    허수아비 코드 짰으니깐 이거 쓰셈 다시는 이러지 마라...

---
+++ Assets/Scripts/Characters/Enemy/Scarecrow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scarecrow : Enemy {
+    public bool neverDie;
+
+    public override void GetDamaged(PlayerAttackInfo attack)
+    {
+        if (Invisible) { return; }
+        float prevHealth = currHealth;
+        currHealth -= attack.damage;
+
+        if (currHealth <= 0)
+        {
+            if (neverDie)
+            {
+                prevHealth = maxHealth;
+                currHealth = maxHealth;
+            }
+            else
+            {
+                Invisible = true;
+                animator.SetTrigger("DeadTrigger");
+                return;
+            }
+        }
+
+        animator.SetTrigger("DamagedTrigger");
+    }
+}
